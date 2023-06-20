aabdc7909e52e4427a6848a77a6b2ad22a02bf3b
osamablal <osamablal2013@gmail.com>
2023-06-20 06:03:53 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,18 @@
  */
 int print_sign(int n)
 {
-If (n > 0)
-{
-_putchar(43);
-Return (1);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0) {
+
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
-Else if (n < 0)
-{
-_putchar(45);
-Return (-1);
-}
-Else
-{
-_putchar(48);
-Return (0);
-}
+
